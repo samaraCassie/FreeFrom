3700be2f1a2e7ff80a4577048fbe4b0478a52fd4
Julian Alves <julianalvesinstinto@gmail.com>
2023-02-09 16:41:57 -0300

    Refatorando e implementando express

---
+++ server.js
@@ -0,0 +1,24 @@
+require('dotenv').config();
+
+const express = require('express');
+const app = express();
+
+const routes = require('./routes');
+const path = require('path');
+const meuMiddleware = require('./src/middlewares/middleware.js');
+
+app.use(express.urlencoded({extended: true}));
+
+app.use(express.static('./public'))
+
+app.set('views', path.resolve(__dirname, 'src', 'views'));
+app.set('view engine', 'ejs');
+
+//Meus middlewares
+app.use(meuMiddleware);
+app.use(routes);
+
+app.listen(3000, () => {
+    console.log('Acessar http://localhost:3000');
+    console.log('Servidor executando na porta 3000');
+});
