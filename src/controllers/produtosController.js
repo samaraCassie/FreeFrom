e963572e66712164c36b20dee53fe0f1f7192890
Julian Alves <julianalvesinstinto@gmail.com>
2023-03-07 14:07:50 -0300

    mudanças na pagina de produtos

--- src/controllers/produtosController.js
+++ src/controllers/produtosController.js
@@ -1,16 +1,3 @@
-const mysql = require('mysql2/promise');
-
-async function conectarBancoDeDados() {
-    const connection = await mysql.createConnection({
-      host: 'localhost',
-      user: 'root',
-      password: '',
-      database: 'freefrom'
-    });
-    return connection;
-}
-const connection = await conectarBancoDeDados();
-const [rows, fields] = await connection.execute('SELECT usuario FROM usuario WHERE usuario = ?' [usuario]);
 exports.paginaProdutos = (req, res) => {
-    res.render('_Produtos', {usuario: 'Sim'});
+    res.render('_Produtos');
 }
