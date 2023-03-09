88191c0c26d9c47b09eb0b37841388a90a327a97
Julian Alves <julianalvesinstinto@gmail.com>
2023-03-09 17:16:53 -0300

    starting the product select

--- src/controllers/produtosController.js
+++ src/controllers/produtosController.js
@@ -1,3 +1,19 @@
+const mysql = require('mysql2/promise');
+
+async function conectarBancoDeDados() {
+    const connection = await mysql.createConnection({
+      host: 'localhost',
+      user: 'root',
+      password: '',
+      database: 'freefrom'
+    });
+    return connection;
+}
+
+const connection = await conectarBancoDeDados();
+const [rows, fields] = await connection.execute('SELECT * FROM produtos');
+
 exports.paginaProdutos = (req, res) => {
     res.render('_Produtos');
 }
+
