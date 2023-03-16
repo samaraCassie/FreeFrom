f1bfd82bcd1da22cfce0cd1af82656449cb5e95c
Julian Alves <julianalvesinstinto@gmail.com>
2023-03-16 16:56:32 -0300

    Beginning of email and password validation message

--- src/controllers/cadastroController.js
+++ src/controllers/cadastroController.js
@@ -1,5 +1,6 @@
 const mysqls = require('mysql2');
 const md5 = require('md5');
+const validator = require('validator');
 
 const connection = mysqls.createConnection({
     host: 'localhost',
@@ -24,26 +25,60 @@ exports.postCadastro = (req, res) => {
   const cidade = req.body.Cidade;
   const uf = req.body.UF;
 
-  
+  var validaEmail;
+  var validaSenha;
 
-  const sql = 'INSERT INTO usuario (email, usuario, senha, data_nascimento, sexo, endereco, numero, cidade, uf) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)';
-  const values = [email, usuario, senha, dataNascimento, sexo, endereço, numero, cidade, uf];
-  connection.query('SELECT * FROM usuario WHERE email = ?', email, (error, results, fields) => {
-      if (error) throw error;
+  if(validator.isEmail(email)){
+    validaEmail = true;
+  }
+  else{
+    validaEmail = false;
+  }
 
-      if (results.length > 0) {
-          // Usuário já existe no banco de dados
-          res.send('Usuário já cadastrado!');
-      } else {
-          connection.query(sql, values, (err, result) => {
-          if (err) {
-              console.error('Erro ao inserir dados no banco de dados: ' + err.stack);
-              return;
-          }
-      
-          console.log('Dados inseridos com sucesso no banco de dados');
-          res.redirect('/_Produtos');
-          });
-      }
-});
+  if(req.body.senha.length > 6 && req.body.senha.length < 30){
+    validaSenha = true;
+  }
+  else{
+    validaSenha = false;
+  }
+  var errors = [];
+
+  if(validaEmail && validaSenha){
+    const sql = 'INSERT INTO usuario (email, usuario, senha, data_nascimento, sexo, endereco, numero, cidade, uf) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)';
+    const values = [email, usuario, senha, dataNascimento, sexo, endereço, numero, cidade, uf];
+        connection.query('SELECT * FROM usuario WHERE email = ?', email, (error, results, fields) => {
+            if (error) throw error;
+
+            if (results.length > 0) {
+                // Usuário já existe no banco de dados
+                res.send('Usuário já cadastrado!');
+            } else {
+                connection.query(sql, values, (err, result) => {
+                if (err) {
+                    console.error('Erro ao inserir dados no banco de dados: ' + err.stack);
+                    return;
+                }
+            
+                console.log('Dados inseridos com sucesso no banco de dados');
+                res.redirect('/_Produtos');
+                });
+            }
+        });
+    }
+    else{
+        if(!validaEmail){
+            errors.push('Email invalido!');
+        }
+        if(!validaSenha){
+            errors.push('Senha invalida!');
+        }
+        if(errors.length>0){
+            req.flash('errors', errors);
+            req.session.save(function() {
+                res.redirect('back');
+                return;
+            });
+            return;
+        }
+    }
 }
