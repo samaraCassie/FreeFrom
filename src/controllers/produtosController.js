bff203a5fb70b63a8875e92012fd9bb15af7f84d
Julian Alves <julianalvesinstinto@gmail.com>
2023-03-18 21:12:41 -0300

    Starting to show database products

--- src/controllers/produtosController.js
+++ src/controllers/produtosController.js
@@ -1,4 +1,32 @@
+const mysqls = require('mysql2');
+
+const connection = mysqls.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'freefrom'
+});
+
 exports.paginaProdutos = (req, res) => {
-    res.render('_Produtos');
+    
+    connection.query('SELECT COUNT(*) as num_produtos FROM produto', (error, results) => {
+        if (error) {
+            console.error('Erro ao executar consulta SQL:', error);
+            return;
+        }
+        const products = [];
+        connection.query('SELECT * FROM produto', (error, result, fields) => {
+        if (error) throw error;
+        result.forEach((row) => {
+                products.push({
+                id: row.id,
+                nome: row.nome,
+                descricao: row.descricao
+                });
+            });
+            res.render('_Produtos', { produtos: products, num_produtos: results[0].num_produtos });
+        });
+    });
 }
+
 
