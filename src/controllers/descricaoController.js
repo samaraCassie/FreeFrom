3354e90cd7ee5e2597e37529c85f46c92327069f
Julian Alves <julianalvesinstinto@gmail.com>
2023-03-25 23:30:18 -0300

    Feat: descricaoProdutos and Produtos, Accessing the product page by id

--- src/controllers/descricaoController.js
+++ src/controllers/descricaoController.js
@@ -1,3 +1,16 @@
+const mysql = require('mysql');
+
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: '',
+  database: 'freefrom'
+});
+
 exports.paginaDesc = (req, res) => {
-    res.render('_descricaoProduto');
+    const id = req.params.id;
+    connection.query('SELECT * FROM produto WHERE id_produto = ?', [id], (error, results, fields) => {
+        if (error) throw error;
+        res.render('_descricaoProduto', { produtos: results });
+    });
 }
