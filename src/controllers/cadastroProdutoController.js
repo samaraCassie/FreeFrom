81e1a26d70b70e2f15b49269998e28bda02b878c
Julian Alves <julianalvesinstinto@gmail.com>
2023-04-13 12:58:45 -0300

    feat: changing the name of the uploaded file in the database

--- src/controllers/cadastroProdutoController.js
+++ src/controllers/cadastroProdutoController.js
@@ -20,7 +20,7 @@ exports.postProduto = (req, res) => {
     const img = req.body.img;
     const preco = req.body.preco;
 
-    const imagem = `img/${img}`;
+    const imagem = `img/${Date.now() + file.originalname}`;
 
     const sql = 'INSERT INTO produto (nome, descricao, preco_unit, qtd_estoque, img, categoria) VALUES (?, ?, ?, ?, ?, ?)';
     const values = [nome, descricao, preco, estoque, imagem, categoria];
@@ -29,9 +29,6 @@ exports.postProduto = (req, res) => {
             console.error('Erro ao inserir dados no banco de dados: ' + err.stack);
             return;
         }
-        console.log(result);
-        console.log(result[0].id_produto);
-
         console.log('Dados inseridos com sucesso no banco de dados');
         res.redirect('/_PerfilLoja');
     });
