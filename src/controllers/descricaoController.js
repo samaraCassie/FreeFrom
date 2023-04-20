a8facbd383bb50ead2c3748c62150e6c7e5d4a9d
Julian Alves <julianalvesinstinto@gmail.com>
2023-04-20 17:12:05 -0300

    feat: making connections with products and sellers

--- src/controllers/descricaoController.js
+++ src/controllers/descricaoController.js
@@ -11,6 +11,11 @@ exports.paginaDesc = (req, res) => {
     const id = req.params.id;
     connection.query('SELECT * FROM produto WHERE id_produto = ?', [id], (error, results, fields) => {
         if (error) throw error;
-        res.render('_descricaoProduto', { produtos: results });
+        
+        connection.query('SELECT * FROM vendedor WHERE id_vendedor = ?', [results[0].id_vendedor], (error, result, fields) => {
+          if(error) throw error;
+
+          res.render('_descricaoProduto', { produtos: results, vendedor: result });
+        });
     });
 }
--- src/controllers/lojaController.js
+++ src/controllers/lojaController.js
@@ -1,3 +1,20 @@
+const mysql = require('mysql');
+
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: '',
+  database: 'freefrom'
+});
+
 exports.paginaLoja = (req, res) => {
-    res.render('_PerfilLoja');
+    const id = req.params.id;
+    connection.query('SELECT * FROM vendedor WHERE id_vendedor = ?', [id], (error, result, fields) => {
+        if(error) throw error
+        connection.query('SELECT * FROM produto WHERE id_vendedor = ?', [id], (err, results, field) => {
+            if(err) throw err
+
+            res.render('_PerfilLoja', {vendedor: result, produtos: results});
+        });
+    })
 }
