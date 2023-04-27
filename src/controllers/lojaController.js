a663b95df03ccb00283ee68cb2aad7e2c507d29b
Julian Alves <julianalvesinstinto@gmail.com>
2023-04-27 16:39:35 -0300

    feat: Start of adding store edit function

--- src/controllers/lojaController.js
+++ src/controllers/lojaController.js
@@ -8,13 +8,33 @@ const connection = mysql.createConnection({
 });
 
 exports.paginaLoja = (req, res) => {
-    const id = req.params.id;
-    connection.query('SELECT * FROM vendedor WHERE id_vendedor = ?', [id], (error, result, fields) => {
-        if(error) throw error
-        connection.query('SELECT * FROM produto WHERE id_vendedor = ?', [id], (err, results, field) => {
-            if(err) throw err
-
-            res.render('_PerfilLoja', {vendedor: result, produtos: results});
+    if(req.session.user){
+        const id = req.params.id;
+        const user = req.session.user;
+        connection.query('SELECT * FROM vendedor WHERE id_vendedor = ?', [id], (error, result, fields) => {
+            if(error) throw error
+            connection.query('SELECT * FROM produto WHERE id_vendedor = ?', [id], (err, results, field) => {
+                if(err) throw err
+                connection.query('SELECT id_usuario FROM vendedor WHERE id_usuario = ?', [user[0].id_usuario], (erro, resultado) => {
+                    if(erro) throw erro
+                    if(resultado != ""){
+                        res.render('_PerfilLoja', {vendedor: result, produtos: results, user: true});
+                    }
+                    else{
+                        res.render('_PerfilLoja', {vendedor: result, produtos: results, user: false});
+                    }
+                });
+            });
         });
-    })
+    }
+    else{
+        const id = req.params.id;
+        connection.query('SELECT * FROM vendedor WHERE id_vendedor = ?', [id], (error, result, fields) => {
+            if(error) throw error
+            connection.query('SELECT * FROM produto WHERE id_vendedor = ?', [id], (err, results, field) => {
+                    if(err) throw err
+                    res.render('_PerfilLoja', {vendedor: result, produtos: results, user: false});
+                });
+            });
+    }
 }
