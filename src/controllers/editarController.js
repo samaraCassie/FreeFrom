33452859f01f3a02a938baab815adbf786ebf125
Julian Alves <julianalvesinstinto@gmail.com>
2023-05-02 15:35:41 -0300

    feat: adding the functionality of only the store seller being able to edit their store

--- src/controllers/editarController.js
+++ src/controllers/editarController.js
@@ -7,10 +7,25 @@ const connection = mysql.createConnection({
   database: 'freefrom'
 });
 exports.editarPage = (req, res) => {
-    const id = req.params.id;
-    connection.query('SELECT * FROM vendedor WHERE id_vendedor = ?', [id], (err, results) => {
-        res.render('_editar', {errado: false, loja: results});
+    const user = req.session.user;
+    if(user){
+    connection.query('SELECT id_vendedor FROM vendedor WHERE id_usuario = ?', [user[0].id_usuario], (err, results) => {
+        if(err) throw err;
+        if(results != ""){
+            if(results[0].id_vendedor == req.params.id){
+                res.render('_editar', {errado: false, loja: true});
+            }
+            else{
+                res.render('_editar', {errado: false, loja: false});
+            }
+        }else{
+            res.render('_editar', {errado: false, loja: false});
+        }
     });
+    }
+    else{
+        res.render('_editar', {errado: false, loja: false});
+    }
 }
 
 exports.postEditar = (req, res) => {
@@ -19,10 +34,12 @@ exports.postEditar = (req, res) => {
     const cnpj = req.body.cnpj;
     const slogan = req.body.slogan;
     const sobre = req.body.sobre;
+
+    console.log(id, nome, cnpj)
     
     connection.query('UPDATE vendedor SET nome_loja = ?, cnpj = ?, slogan = ?, sobre = ? WHERE id_vendedor = ?', [nome, cnpj, slogan, sobre, id], (erro, result) => {
         if(erro) throw erro
     
-        res.redirect('_Perfilloja');
+        res.redirect('/_Perfilloja/' + id);
     });
 }
