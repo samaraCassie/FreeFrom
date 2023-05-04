db38b49380c227aa9ebdb14431ce6dc7afccd389
Julian Alves <julianalvesinstinto@gmail.com>
2023-05-04 15:51:26 -0300

    feat: adding controller to change image

---
+++ src/controllers/mudarCapaController.js
@@ -0,0 +1,23 @@
+const mysql = require('mysql');
+
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: '',
+  database: 'freefrom'
+});
+
+exports.mudarCapa = (req, res) => {
+    const path = req.file.path;
+    const id = req.body.id;
+
+    let imagem = path.slice(7);
+
+    console.log(imagem, id);
+
+    connection.query('UPDATE vendedor SET back_img = ? WHERE id_vendedor = ?', [imagem, id], (err, result) => {
+        if(err) throw err;
+
+        res.redirect('/_PerfilLoja/' + id);
+    });
+}
