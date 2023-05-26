e94f12897cf8af1e5e2a8d511bf474b7c7e30ba9
Samara Cassie <samaracassie@outlook.com>
2023-05-26 14:24:47 -0300

    fex: change port

--- chat/backend/index.js
+++ chat/backend/index.js
@@ -2,7 +2,7 @@ const { serverHttp, app } = require("./server");
 const path = require("path");
 require("./chat");
 
-const port = 3003;
+const port = 3006;
 
 app.get("/", (request, response) => {
   response.sendFile(path.join(__dirname, "..", "frontend", "index.html"));
